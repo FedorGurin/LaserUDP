6f4b2ea2ebd52f36581c1eb114e210b2e451f6a2
Fedor <fedor@localhost.localdomain>
2019-08-13 19:05:20 +0300

    новые правки

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <QCoreApplication>
 
+#include <QUdpSocket>
+#include <QByteArray>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    QUdpSocket udpSocketRec;
+
+    bool result = udpSocketRec.bind(6080,QAbstractSocket::ReuseAddressHint);
+
+//    connect(&udpSocketRec, SIGNAL(readyRead()),this, SLOT(processPendingDatagrams()));
 
     return a.exec();
 }
